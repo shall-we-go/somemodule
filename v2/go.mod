658c66a575bc4bafe1225dbff691104161ce0d6a
choopong <cchoopong@gmail.com>
2021-01-13 12:56:18 +0700

    create v2

---
+++ v2/go.mod
@@ -0,0 +1,8 @@
+module github.com/shall-we-go/somemodule/v2
+
+go 1.15
+
+require (
+	rsc.io/quote v1.5.2
+	rsc.io/quote/v3 v3.1.0
+)
---
+++ v2/hello.go
@@ -0,0 +1,18 @@
+package somemodule/v2
+
+import (
+	quote "rsc.io/quote"
+	quoteV3 "rsc.io/quote/v3"
+)
+
+func Hello() string {
+	return quote.Hello()
+}
+
+func HelloWithVersion() string {
+	return Hello() + " v2.0.0"
+}
+
+func Proverb() string {
+	return quoteV3.Concurrency()
+}
